4f657fa2c7fc75314725c4d91c81677059882b0f
Jennings Anderson <jennings.anderson@colorado.edu>
2017-10-27 14:19:03 -0600

    parallel db scan clustering in node

--- util/cluster-worker.js
+++ util/cluster-worker.js
@@ -1,15 +1,94 @@
+'use strict';
+
 var dbscan = require('@turf/clusters-dbscan')
+var turfCentroid = require('@turf/centroid')
+var turfDistance = require('@turf/distance')
+var fs     = require('fs')
+var _      = require('lodash')
 
+var fileToParse = process.argv[2];
 
-module.exports = function(geojson, params, done){
+var geojson;
 
-// create random points with random z-values in their properties
-var points = turf.random('point', 100, {
-  bbox: [0, 30, 20, 50]
-});
-var distance = 100;
-var clustered = turf.clustersDbscan(points, distance);
+//console.log("In worker: " + process.pid + " with: " + fileToParse)
 
+try{
+  geojson = JSON.parse(fs.readFileSync(fileToParse))
 
-done()
+  //Get only features with geometry and cluster them
+  var withGeometries = []
+  var withoutGeometries = []
+  geojson.features.map(function(f){
+    if(f.geometry){
+      withGeometries.push({
+        type      : 'Feature',
+        geometry  : f.geometry,
+        properties: {
+          user: f.properties.user,
+          text: f.properties.text,
+          date: f.properties.date,
+          speed: f.properties.speed || "",
+          timeD: f.properties.time_delta || ""
+        }
+     })
+   }else{
+     withoutGeometries.push(f)
+   }
+  })
+
+  var distance = 100;
+  var clustered = dbscan({
+    type:'FeatureCollection',
+    features: withGeometries
+  }, distance);
+
+  var clusterGroups = _.groupBy(clustered.features,function(f){return f.properties.cluster})
+  var clusterCenters = {}
+
+  Object.keys(clusterGroups).forEach(function(cID){
+    if(cID){
+      clusterCenters[cID] = turfCentroid({type:'FeatureCollection',features:clusterGroups[cID]})
+    }
+  })
+
+  //Break here if we haven't been able to classify anything
+
+  //Now we merge all the properties back together, sorted by time
+  var newFeatures = withoutGeometries.concat(clustered.features).map(function(f){
+    f.properties.date = new Date(f.properties.date)
+    return f
+  })
+  var sortedFeatures = _.sortBy(newFeatures, function(f){return f.properties.date})
+
+  //Calculate Speeds
+  var prev, cur, speed, timeDelta
+  for(var i in sortedFeatures){
+    cur  = sortedFeatures[i]
+    if (i>0){
+      prev = sortedFeatures[i-1]
+      timeDelta = Math.floor((cur.properties.date - prev.properties.date)/1000)
+      cur.properties.timeDelta = timeDelta
+      //If both have points, use them:
+      if (prev.geometry && cur.geometry){
+        speed = turfDistance(prev, cur, 'miles') / (timeDelta/3600)
+      }else{ //TODO: Be smart and use other points in the temporal cluster?
+        speed = null
+      }
+      cur.properties.speed = speed
+    }
+  }
+
+  console.log(JSON.stringify(sortedFeatures, null, 2))
+
+  process.send({
+    success: true,
+    finished: fileToParse
+  })
+
+}catch(err){
+  console.warn("Error on file: ", fileToParse)
+  throw err
 }
+
+//Is this required at the end?
+process.disconnect();
--- util/index.js
+++ util/index.js
@@ -1,16 +1,44 @@
-var child_process = require('child_process');
+'use strict';
 
-var numchild  = require('os').cpus().length;
-var done      = 0;
+const fork = require('child_process').fork;
+const fs = require('fs')
+const path = require('path')
+const async = require('async');
 
-for (var i = 0; i < numchild; i++){
-  var child = child_process.fork('./child');
-  child.send((i + 1) * 1000);
+const numWorkers  = require('os').cpus().length - 2;
+const inputDir = './test'
+
+//Create global path
+var dirListing = fs.readdirSync(inputDir)
+var inputFiles = dirListing.map(function(f){
+  return path.join( __dirname + '/test/',f)
+})
+
+console.warn("Found "+inputFiles.length+" input files")
+
+var done = 0;
+
+//Setup the queue
+var q = async.queue(function (filePath, callback) {
+
+  //Call the process
+  var child = fork('./cluster-worker', [filePath], {silent: false});
+
+  //Status Logging
   child.on('message', function(message) {
-    console.log('[parent] received message from child:', message);
-    done++;
-    if (done === numchild) {
-      console.log('[parent] received all results');
+    if (message.success){
+      process.stderr.write("\rProcessed: "+ ++done + " users");
     }
+    callback()
   });
+}, numWorkers);
+
+// assign a callback
+q.drain = function() {
+  console.warn('\nQueue has been drained');
 }
+
+//Add all the files to be processed to the queue:
+q.push(inputFiles.splice(0,1),function(){
+  return;
+});
